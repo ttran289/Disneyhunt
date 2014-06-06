18eaf33195ca4217fae9bafc5dd93e7a6f0eb784
ttran289 <thachtran289@gmail.com>
2014-06-06 16:29:16 -0700

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV['disneyhunt'],
+    :access_key_id => ENV['AKIAION4JXMSTZPVL4KA'],
+    :secret_access_key => ENV['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']
   }
 }
 
